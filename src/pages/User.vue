ece847688175c089d086195ff63978d51db045b2
lijiaming123 <852930342@qq.com>
2018-04-16 23:22:22 +0800

    user

--- src/pages/User.vue
+++ src/pages/User.vue
@@ -1,9 +1,19 @@
 <template>
-	
+	<v-foot></v-foot>
 </template>
 
 <script>
-	
+	import Foot from '../pages/foot.vue';
+	export default{
+		components:{
+			'v-foot':Foot
+		},
+		data(){
+			return{
+
+			}
+		},
+	}
 </script>
 
 <style>
